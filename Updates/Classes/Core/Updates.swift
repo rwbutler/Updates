10dd492ec5380babdfbc7712d1de7f8c98798a1d
Nick Brook <nrbrook@gmail.com>
2024-10-31 16:30:06 +0000

    Fix whitespace

--- Updates/Classes/Core/Updates.swift
+++ Updates/Classes/Core/Updates.swift
@@ -197,10 +197,8 @@ public class Updates {
 import UIKit
 
 public extension Updates {
-    
     static func checkForUpdates(completion: @escaping (UpdatesResult) -> Void) {
         Updates.checkForUpdates(currentOSVersion: UIDevice.current.systemVersion, completion: completion)
     }
-    
 }
 #endif
